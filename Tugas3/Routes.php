fec7d8506ef7ae38e31b201dd130f3bac9ddfeb9
Pemweb2 <armamk43@gmail.com>
2024-03-06 15:13:23 +0700

    minggu3

---
+++ Tugas3/Routes.php
@@ -0,0 +1,12 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/About', 'Page::about');
+$routes->get('/Contact', 'Page::contact');
+$routes->get('/Faqs', 'Page::faqs');
+$routes->setAutoRoute(true);
